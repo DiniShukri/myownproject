4e04b7f6e75fa8c6ecc34001bf35bcbe89966380
DiniShukri <127168634+DiniShukri@users.noreply.github.com>
2023-03-07 15:45:13 +0800

    Update myfirstproject.py

--- myfirstproject.py
+++ myfirstproject.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('Advertising.csv')
-df2=df.drop('',axis=1)
+df2=df.drop(' ',axis=1)
 
 st.header("My first Project")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
 if show:
-    st.write(df)
+    st.write(df2)
     
